e1ae7b011868b7e66422f1021be00626fabbc4a6
Paola Ferrario <paola.ferrario@gmail.com>
2018-11-19 12:22:06 +0100

    Add functions used in the topology analysis.

---
+++ utils/topology_functions.py
@@ -0,0 +1,58 @@
+"""
+Set of functions used in the topology analysis
+"""
+
+import numpy as np
+from .. histo_functions import exp, gauss
+
+### Errors from the fit, bin by bin
+
+def var_on_signal_events(f, x):
+    #g = f.values[2]/(2*np.pi)**.5/f.values[4] * np.exp(-0.5*(x-f.values[3])**2./f.values[4]**2.)
+    ### derivative of gaussian with respect to the three variable: amplitude, mean and sigma
+    der_g_amp = 1/(2*np.pi)**.5/f.values[4] * np.exp(-0.5*(x-f.values[3])**2./f.values[4]**2.)
+    der_g_mu = f.values[2]/(2*np.pi)**.5 * np.exp(-0.5*(x-f.values[3])**2./f.values[4]**2.) * (x-f.values[3])/f.values[4]**3
+    der_g_sigma = f.values[2] * np.exp(-0.5*(x-f.values[3])**2./f.values[4]**2.)*(x-f.values[3])**2/(2*np.pi)**.5/f.values[4]**4 - f.values[2] * np.exp(-0.5*(x-f.values[3])**2./f.values[4]**2.)/(2*np.pi)**.5/f.values[4]**2
+
+    sigma2_Ns = der_g_amp**2 * f.cov[2, 2] + der_g_mu**2 * f.cov[3, 3] + der_g_sigma**2 * f.cov[4, 4]
+    sigma2_Ns += 2 * der_g_amp * der_g_mu * f.cov[2,3]
+    sigma2_Ns += 2 * der_g_amp * der_g_sigma * f.cov[2,4]
+    sigma2_Ns += 2 * der_g_mu * der_g_sigma * f.cov[3,4]
+
+    return sigma2_Ns
+
+
+def var_on_background_events(f, x):
+    #e = f.values[0] * np.exp(x/f.values[1])
+    ### derivative of exponential with respect to the two variables: amplitude and decay constant
+    der_e_a0 = np.exp(x/f.values[1])
+    der_e_tau = - f.values[0] * np.exp(x/f.values[1]) * x/f.values[1]**2
+
+    sigma2_Nb = der_e_a0**2 * f.cov[0,0] + der_e_tau**2 * f.cov[1,1]
+    sigma2_Nb += 2 * der_e_a0 * der_e_tau * f.cov[0,1]
+
+    return sigma2_Nb
+
+
+def find_fractions(x, fit_result, e_min, e_max, e_min_plot, e_max_plot, nbins_plot):
+    low_bin = np.digitize(e_min, x)
+    high_bin = np.digitize(e_max, x)
+
+    bin_width = (e_max_plot - e_min_plot) / nbins_plot
+
+    s = b = 0.
+    var_s = var_b = 0
+    for i in range(low_bin, high_bin+1):
+        centre_value = e_min_plot + i * bin_width + bin_width/2
+        s += gauss(centre_value, fit_result.values[2], fit_result.values[3], fit_result.values[4])
+        b += exp(centre_value, fit_result.values[0], fit_result.values[1])
+
+        var_s += var_on_signal_events(fit_result, centre_value)
+        var_b += var_on_background_events(fit_result, centre_value)
+
+    fs = s/(s+b)
+    fb = b/(s+b)
+    err_fs = np.sqrt((b/(s+b)**2)**2*var_s + (s/(s+b)**2)**2*var_b)
+    err_fb = np.sqrt((s/(s+b)**2)**2*var_b + (b/(s+b)**2)**2*var_s)
+
+    return(fs, fb, err_fs, err_fb)
